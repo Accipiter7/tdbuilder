8a7d142dfa2d388c75e205f7caf44322038ba673
Accip <64995687+Accipiter7@users.noreply.github.com>
2020-11-16 14:55:39 +0530

    Update waho.sh

--- waho.sh
+++ waho.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 DEBIAN_FRONTEND=noninteractive
-sudo apt-get install make git zlib1g-dev libssl-dev gperf php cmake clang-11 libc++-dev libc++abi-dev
+sudo apt-get install make git zlib1g-dev libssl-dev gperf php cmake clang-10 libc++-dev libc++abi-dev
 git clone https://github.com/tdlib/td.git
 cd td
 rm -rf build
@@ -8,6 +8,6 @@ mkdir build
 cd build
 export CXXFLAGS="-stdlib=libc++"
 CC=/usr/bin/clang-10 CXX=/usr/bin/clang++-10 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../tdlib -DTD_ENABLE_LTO=ON -DCMAKE_AR=/usr/bin/llvm-ar-10 -DCMAKE_NM=/usr/bin/llvm-nm-10 -DCMAKE_OBJDUMP=/usr/bin/llvm-objdump-10 -DCMAKE_RANLIB=/usr/bin/llvm-ranlib-10 ..
-cmake --build . --target tdjson
+cmake --build . --target tdjson_static
 ls -alh
 pwd
