b40eb17b323c203393279ac07752307e6b43f086
Accip <64995687+Accipiter7@users.noreply.github.com>
2020-11-16 12:00:54 +0530

    Update waho.sh

--- waho.sh
+++ waho.sh
@@ -10,3 +10,4 @@ export CXXFLAGS="-stdlib=libc++"
 CC=/usr/bin/clang-10 CXX=/usr/bin/clang++-10 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../tdlib -DTD_ENABLE_LTO=ON -DCMAKE_AR=/usr/bin/llvm-ar-10 -DCMAKE_NM=/usr/bin/llvm-nm-10 -DCMAKE_OBJDUMP=/usr/bin/llvm-objdump-10 -DCMAKE_RANLIB=/usr/bin/llvm-ranlib-10 ..
 cmake --build . --target tdjson
 ls -alh
+pwd
